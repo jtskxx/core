a59145a806435fcd83c9be2dd28eeab48f637734
cyber-pc <cyber-pc@blue-pc.localhost>
2025-10-13 16:46:44 +0700

    Add debug message for vote count

--- src/public_settings.h
+++ src/public_settings.h
@@ -65,7 +65,7 @@ static_assert(AUTO_FORCE_NEXT_TICK_THRESHOLD* TARGET_TICK_DURATION >= PEER_REFRE
 #define VERSION_C 0
 
 // Epoch and initial tick for node startup
-#define EPOCH 182
+#define EPOCH 180
 #define TICK 34270000
 #define TICK_IS_FIRST_TICK_OF_EPOCH 1 // Set to 0 if the network is restarted during the EPOCH with a new initial TICK
 
